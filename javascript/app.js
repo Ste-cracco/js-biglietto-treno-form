97cee75b2ffff3a0e6b0c46d691018f80878ac5f
Ste-cracco <s.cracco@yahoo.it>
2022-07-21 17:45:21 +0200

    Prezzi dei biglietti calcolati correttamente

--- javascript/app.js
+++ javascript/app.js
@@ -22,32 +22,35 @@
 //             }       
 
 
-// const buttonElement = document.querySelector('input[type="submit"]'); 
-// buttonElement.addEventListener('click', function ()  { 
-//     const inputElement = document.querySelector('input[name="nome_cognome"]');  
-//     const nomePasseggero = inputElement.value; 
-//     console.log(nomePasseggero); 
-// })
 
 const buttonElement = document.querySelector('input[type="submit"]'); 
 buttonElement.addEventListener('click', function ()  { 
     const inputNomeCognome = document.querySelector('input[name="nome_cognome"]'); 
     const nomePasseggero = inputNomeCognome.value;
+
     const inputKilometri = document.querySelector('input[name="kilometri"]');
-    const prezzoBase = inputKilometri.value * 0.21;
-    let inputEta = document.querySelector('select[name="fascia_eta"]');
+    const prezzoBase = parseFloat(inputKilometri.value * 0.21);
+
+    let inputEta = document.getElementById('fascia_eta');
     let etaPasseggero = inputEta.value;
-    if (etaPasseggero < 18) {
-        let scontoMinorenni = (prezzoBase / 100) * 20;
-        let prezzoMinorenni = prezzoBase - scontoMinorenni
-        console.log ('Il costo del biglietto è di €' + prezzoMinorenni.toFixed(2))
+
+    if (isNaN(kilometri)) {
+           console.log ('Inserisci un numero di km valido')
+    }   
+    else if (etaPasseggero == 'minorenne') {
+            let scontoMinorenni = (prezzoBase / 100) * 20;
+            let prezzoMinorenni = prezzoBase - scontoMinorenni;
+            console.log(nomePasseggero, 'Il prezzo del biglietto è di €' + prezzoMinorenni);
     }
-    if (etaPasseggero > 65) {
+    else if (etaPasseggero == 'over65') {
         let scontoOver65 = (prezzoBase / 100) * 40;
         let prezzoOver65 = prezzoBase - scontoOver65;
-        console.log ('Il costo del biglietto è di € ' + prezzoOver65.toFixed(2))
+        console.log(nomePasseggero, 'Il prezzo del biglietto è di €' + prezzoOver65);
     }
-    
+    else if (etaPasseggero == 'maggiorenne') {
+        let prezzoMaggiorenni = prezzoBase;  
+        console.log(nomePasseggero, 'Il prezzo del biglietto è di €' + prezzoMaggiorenni);
+    }    
 })
 
 
